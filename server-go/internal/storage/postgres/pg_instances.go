dac6c57c7bd146ce1947bc3fa9902ea1a0ad7513
dukevuzz <ducvuminh.dev@gmail.com>
2026-09-20 16:41:03 +0700

    EXC-400: project deletion no longer clears a table that no longer exists

--- server-go/internal/storage/postgres/pg_instances.go
+++ server-go/internal/storage/postgres/pg_instances.go
@@ -401,7 +401,6 @@ var projectOwnedTables = []string{
 	"rls_policies",
 	"column_policies",
 	"table_grants",
-	"project_exposure_settings",
 	"project_cors_settings",
 	"edge_function_settings",
 	"edge_functions",
